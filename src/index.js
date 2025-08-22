a392d34aa6ebfbcd9bc88ed6f943798c968ef650
AikoWada314 <aiko.lifestyle314@gmail.com>
2025-08-22 22:52:20 +0900

    fix_CSS Moduleを適用

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './components/Header';
-import BlogList from './pages/BlogList';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./Home/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      <BlogList />
+      <Home />
     </BrowserRouter>
   </React.StrictMode>
 );
